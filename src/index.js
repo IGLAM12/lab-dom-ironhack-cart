64d90842e6a46aabc58f0728fa0166f0bf76de11
IGLAM12 <136024837+IGLAM12@users.noreply.github.com>
2023-09-07 00:09:06 -0300

    cart

--- src/index.js
+++ src/index.js
@@ -1,42 +1,68 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = +product.querySelector('.price span').textContent;
+  const quantity = +product.querySelector('.quantity input').value;
+  const subtotalTag = product.querySelector('.subtotal span');
+  const calculate = price * quantity;
+  subtotalTag.textContent = calculate;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  document.querySelectorAll('.product').forEach(updateSubtotal);
+  const subtotals = document.querySelectorAll('.subtotal span');
+  const totalTag = document.querySelector('#total-value span');
+  let total = 0;
+  subtotals.forEach(subtotal => {
+    total += +subtotal.textContent;
+  });
+  totalTag.textContent = total;
 }
-
-// ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const parentRow = target.parentElement.parentElement;
+  parentRow.remove();
 }
 
-// ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  const addTittle = document.querySelector('.create-product input[type="text"]');
+  const addPrice = document.querySelector('.create-product input[type="number"]');
+  if (addTittle.value === '' || addPrice.value === '') {
+    alert('Por favor, completa todos los campos antes de agregar un producto.');
+    return;
+  }
+  let newTittle = addTittle.value;
+  let newPrice = addPrice.value;
+  let newProduct = document.createElement('tr');
+  newProduct.classList.add('product');
+  newProduct.innerHTML = `
+    <td class="name">
+      <span>${newTittle}</span>
+    </td>
+    <td class="price">$<span>${newPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  `;
+  const tbody = document.querySelector('tbody');
+  tbody.appendChild(newProduct);
+
+  const removeBtn = newProduct.querySelector('.btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeProducts = document.getElementsByClassName('btn-remove');
+  for (removeBtn of removeProducts) {
+    removeBtn.addEventListener('click', removeProduct);
+  }
+
+  const createProducts = document.getElementById('create');
+  createProducts.addEventListener('click', createProduct);
 });
